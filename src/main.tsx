7cae792736255959d654628aae5489ca9d8e0bc3
siddharthasarma <sidduinevitable@gmail.com>
2023-10-17 12:25:10 +0530

    move router config to separate file

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
+import { router as MyRouter } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+     <RouterProvider router={MyRouter} />
   </React.StrictMode>,
 )
---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import App from './App.tsx'
+
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
